f44b51cccc9a58afd8f261df6ca5a3da22c45c9b
Miki-0120 <miki.tanaka07@gmail.com>
2025-08-11 23:00:59 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import UserTable from "./components/UserTable"
+
+function App() {
+
+  return (
+    <div style={{ padding: '20px' }}>
+      <h1>ユーザー一覧</h1>
+      <UserTable />
+    </div>
+  );
+}
+
+export default App;
